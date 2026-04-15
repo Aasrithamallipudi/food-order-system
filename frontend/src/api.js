c71fdfaabb35f8a59fa8f737cab5efe2c5f9aef2
aasritha <aasrithamallipudi@gmail.com>
2026-04-15 21:16:43 +0530

    initial commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api"
+  baseURL: "https://food-order-system-3.onrender.com/api"
 });
 
 // Add request interceptor for debugging
